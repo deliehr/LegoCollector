b4caf572f3adebac1fef7d3309e2c223e6280a0a
Dominik Liehr <dominik_liehr@me.com>
2023-07-14 13:03:26 +0200

    - initial commit

---
+++ LegoCollector/LegoCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegoCollectorApp.swift
+//  LegoCollector
+//
+//  Created by Dominik Liehr on 14.07.23.
+//
+
+import SwiftUI
+
+@main
+struct LegoCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
